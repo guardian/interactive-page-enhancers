9d0f05b69f569187530be573200e1005eb444598
andymason <a.w.mason@gmail.com>
2016-01-15 12:11:42 +0000

    Add check before using styles

--- src/arabic/boot.js
+++ src/arabic/boot.js
@@ -4,25 +4,27 @@ define([], function() {
     	boot: function(el) {
 
     		var contentHeadline = document.querySelector('.content__main-column');
-			contentHeadline.style.direction = "rtl";
+            if (contentHeadline) {
+			 contentHeadline.style.direction = 'rtl';
+             contentHeadline.lang = 'ar';
+            }
 
 			var contentStandfirst = document.querySelector('.content__standfirst');
-			contentStandfirst.style.direction = "rtl";
+            if (contentStandfirst) {
+			 contentStandfirst.style.direction = 'rtl';
+             contentStandfirst.lang = 'ar';
+            }
 
 			var contentBody = document.querySelector('.content__article-body');
-			contentBody.style.direction = "rtl";
-
-			var contentBody = document.querySelector('.content__article-body');
-			contentBody.lang = "ar";
-
-			var contentHeadline = document.querySelector('.content__main-column');
-			contentHeadline.lang = "ar";
-
-			var contentStandfirst = document.querySelector('.content__standfirst');
-			contentStandfirst.lang = "ar";
+            if (contentBody) {
+			 contentBody.style.direction = 'rtl';
+             contentBody.lang = 'ar';
+            }
 
 			var contentPicshares = document.querySelector('.block-share--article');
-			contentPicshares.style.left = "0px";
+            if (contentPicshares) {
+			 contentPicshares.style.left = '0px';
+            }
 			
     	}
     }
